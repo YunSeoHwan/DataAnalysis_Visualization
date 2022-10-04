48752a37bc0b5be56b7516f9182357d3edfcf8c6
YunSeoHwan <dbstjghks@naver.com>
2022-10-04 23:19:41 +0900

    weather_data output

--- data_anlasys/data_anl_weather.py
+++ data_anlasys/data_anl_weather.py
@@ -1,2 +1,11 @@
-a = 123
-print(a)
+import csv
+
+f = open('data_anlasys\seoul_weather.csv', 'r', encoding='cp949')
+
+# data에 csv객체 저장(, 기준으로)
+# data = csv.reader(f, delimiter=',')
+
+data = csv.reader(f)
+for row in data:
+    print(row)
+f.close()
