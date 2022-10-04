ecedddaac7963ef0bd7a71d45d6b94e781c6e27d
YunSeoHwan <dbstjghks@naver.com>
2022-10-04 23:25:22 +0900

    header 설정

--- data_anlasys/data_anl_weather.py
+++ data_anlasys/data_anl_weather.py
@@ -6,6 +6,9 @@ f = open('data_anlasys\seoul_weather.csv', 'r', encoding='cp949')
 # data = csv.reader(f, delimiter=',')
 
 data = csv.reader(f)
+
+# 헤더 저장(속성값 저장) next : 첫 행을 읽으면서 다음 위치로 이동
+header = next(data)
 for row in data:
     print(row)
 f.close()
